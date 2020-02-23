ec6a203e75f0741de9a66daf56ff808afc95d7bd
code-red-panda <jake@coderedpanda.com>
2020-02-23 14:32:51 +0000

    backup encryption

--- data/backup/nextcloud_backup.sh
+++ data/backup/nextcloud_backup.sh
@@ -9,6 +9,9 @@ BACKUP_DIR="/backups/$TS"
 TAR_SRC="/var/lib/docker/volumes/* /opt/cloud/data/* /opt/cloud/.env /root/.my.cnf /etc/cron.d/nextcloud_crons --exclude /var/lib/docker/volumes/cloud_percona_datadir"
 TAR_TRG="$BACKUP_DIR/nextcloud_backup.tar.gz"
 MYDUMPER_DIR="$BACKUP_DIR/mydumper"
+BACKUP_DIR_TAR_TRG="$BACKUP_DIR.tar.gz"
+BACKUP_DIR_TAR_GPG="$BACKUP_DIR_TAR_TRG.gpg"
+GPG_EMAIL="nextcloud@coderedpanda.com"
 
 
 
@@ -34,7 +37,7 @@ echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO::>>>>>>>>>>>>>>>>>> BACKUP STATUS: [ 
 
 
 
-echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO:: Putting Nextcloud into maintenance mode..."
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Putting Nextcloud into maintenance mode..."
 docker exec -u www-data nextcloud php occ maintenance:mode --on >> $DETAIL_LOG 2>&1
   if test ! $? = 0
   then
@@ -45,7 +48,7 @@ docker exec -u www-data nextcloud php occ maintenance:mode --on >> $DETAIL_LOG 2
 
 
 
-echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO:: Creating backup directory $BACKUP_DIR..."
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Creating backup directory $BACKUP_DIR..."
 mkdir -p $BACKUP_DIR >> $DETAIL_LOG 2>&1
 mkdir -p $MYDUMPER_DIR >> $DETAIL_LOG 2>&1
   if test ! $? = 0
@@ -58,7 +61,7 @@ mkdir -p $MYDUMPER_DIR >> $DETAIL_LOG 2>&1
 
 
 
-echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO:: Tar'ing Docker volumes $TAR_SRC..."
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Tar'ing Docker volumes $TAR_SRC..."
 tar czf $TAR_TRG $TAR_SRC >> $DETAIL_LOG 2>&1
   if test ! $? = 0
   then
@@ -70,7 +73,7 @@ tar czf $TAR_TRG $TAR_SRC >> $DETAIL_LOG 2>&1
 
 
 
-echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO:: Taking Mydumper backup..."
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Taking Mydumper backup..."
 mydumper --outputdir=$MYDUMPER_DIR --regex='^((nextcloud\.|mysql\.user))' --threads=2 --chunk-filesize=5120 --build-empty-files  --compress --verbose=3 --trx-consistency-only >> $DETAIL_LOG 2>&1
   if test ! $? = 0
   then
@@ -82,11 +85,44 @@ mydumper --outputdir=$MYDUMPER_DIR --regex='^((nextcloud\.|mysql\.user))' --thre
 
 
 
-echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO:: Taking Nextcloud out of maintenance mode..."
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Taking Nextcloud out of maintenance mode..."
 docker exec -u www-data nextcloud php occ maintenance:mode --off >> $DETAIL_LOG 2>&1
   if test ! $? = 0
   then
       echo "`date "+%Y-%m-%d %H:%M:%S UTC"` ERROR:: Nextcloud was not taken out of maintenance mode. Check the details log $DETAIL_LOG. Exiting."
+      echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO::>>>>>>>>>>>>>>>>>> BACKUP STATUS: [ FAILED ]"
+      exit 1
+  fi
+
+
+
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Tar'ing backup directory $BACKUP_DIR..."
+tar czf $BACKUP_DIR_TAR_TRG $BACKUP_DIR >> $DETAIL_LOG 2>&1
+  if test ! $? = 0
+  then
+      echo "`date "+%Y-%m-%d %H:%M:%S UTC"` ERROR:: Tar'ing backup directory did not complete OK. Check the details log $DETAIL_LOG."
+      echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO::>>>>>>>>>>>>>>>>>> BACKUP STATUS: [ FAILED ]"
+      exit 1
+  fi
+
+
+
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Encrypting backup $BACKUP_DIR_TAR_TRG..."
+gpg --output $BACKUP_DIR_TAR_GPG --encrypt --recipient $GPG_EMAIL $BACKUP_DIR_TAR_TRG
+  if test ! $? = 0
+  then
+      echo "`date "+%Y-%m-%d %H:%M:%S UTC"` ERROR:: Encrypting backup did not complete OK."
+      echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO::>>>>>>>>>>>>>>>>>> BACKUP STATUS: [ FAILED ]"
+      exit 1
+  fi
+
+
+
+echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO STEP 1/7:: Cleaning up and removing untar'd and unecrypted backups $BACKUP_DIR and $BACKUP_DIR_TAR_TRG..."
+rm -rf $BACKUP_DIR $BACKUP_DIR_TAR_TRG >> $DETAIL_LOG 2>&1
+  if test ! $? = 0
+  then
+      echo "`date "+%Y-%m-%d %H:%M:%S UTC"` ERROR:: Removing backups did not complete OK. Check the details log $DETAIL_LOG."
       echo "`date "+%Y-%m-%d %H:%M:%S UTC"` INFO::>>>>>>>>>>>>>>>>>> BACKUP STATUS: [ FAILED ]"
       exit 1
   fi
